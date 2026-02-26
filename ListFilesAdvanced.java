07d40e6e511e2f641004e18b3eedbb6e4edc9315
Loic Magnette <loic.magnette@gmail.com>
2026-02-26 12:09:04 +0000

    [STEP-2] multiple file

---
+++ ListFilesAdvanced.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import files.FileMetadata;
+import files.FilePrinterService;
+
+public class ListFilesAdvanced {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please enter the path you want to use to list file (ie: /bin/):");
+        var path = "";
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            path = reader.readLine();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        // list files in a directory
+        var dir = new File(path);
+        if (!dir.exists() || !dir.isDirectory()) {
+            System.out.println("The path you entered is not a valid directory.");
+            return;
+        }
+        var files = Arrays.stream(dir.listFiles()).map(FileMetadata::fromFile).toList();
+        new FilePrinterService().prettyPrintFiles(files);
+    }
+}
