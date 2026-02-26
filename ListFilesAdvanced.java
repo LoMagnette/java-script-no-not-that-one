740e13fc1407dcb8920167c22be6913309d33e33
Loic Magnette <loic.magnette@gmail.com>
2026-02-26 12:13:55 +0000

    [STEP-4] simpler IO

--- ListFilesAdvanced.java
+++ ListFilesAdvanced.java
@@ -1,27 +1,21 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 
 import files.FileMetadata;
 import files.FilePrinterService;
 
+static String DEFAULT_PATH = "./";
+
 void main() {
 
-    System.out.println("Please enter the path you want to use to list file (ie: /bin/):");
-    var path = "";
-    try {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        path = reader.readLine();
-    } catch (IOException ioe) {
-        ioe.printStackTrace();
-    }
+    var path = IO.readln("Please enter the path you want to use to list file (ie: /bin/):");
 
-    // list files in a directory
+    if(path == null || path.isEmpty())
+        path = DEFAULT_PATH;
+
     var dir = new File(path);
     if (!dir.exists() || !dir.isDirectory()) {
-        System.out.println("The path you entered is not a valid directory.");
+        IO.println("The path you entered is not a valid directory.");
         return;
     }
     var files = Arrays.stream(dir.listFiles()).map(FileMetadata::fromFile).toList();
